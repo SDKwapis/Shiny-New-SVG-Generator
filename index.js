325a3f853cd4fb7e5def8d3ccbbacb812ec3ea96
Stephan Kwpais <stephankwpais@Stephans-Mac-mini.local>
2024-07-06 16:02:15 -0400

    wrote function that generates the svg file, edited shape classes

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
-const {Circle, Triangle, Square} = require('./lib/shapes.js');
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
 const inquirer = require("inquirer");
-const fs = require('fs');
-
+const fs = require("fs");
 
 inquirer
   .prompt([
     {
-        type: 'maxlength-input',
-        name: 'title',
-        message: 'Enter up to three leters for your logo name.',
-        maxLength: 3
+      type: "input",
+      name: "title",
+      message: "Enter three letters for your logo name.",
+      validate: input => input.length <= 3 || "Logo name must be three letters."
     },
     {
-        type: "list",
-        message: "What color would you like your text to be?",
-        name: "colors",
-        choices: ["red", "blue", "green", "yellow", "cyan", "magenta"],
-      },
+      type: "input",
+      message: "Enter the name or hex code of the color you would like your text to be",
+      name: "textColor"
+    },
     {
       type: "list",
       message: "What shape would you like to use for your logo?",
-      name: "shapes",
-      choices: ["Circle", "Triangle", "Square"],
+      name: "shape",
+      choices: ["Circle", "Triangle", "Square"]
     },
     {
-        type: "list",
-        message: "What color would you like your shape to be?",
-        name: "colors",
-        choices: ["red", "blue", "green", "yellow", "cyan", "magenta"],
+      type: "input",
+      message: "Enter the name or hex code of the color you would like your shape to be",
+      name: "shapeColor"
     },
-    {
-        message: "Generated logo.svg",
-        name: "complete"
-    }
   ])
+  .then((data) => {
+    if (data.shape === "Square") {
+      console.log("Generating your logo now!");
+      const square = new Square(data.shapeColor, data.textColor, data.title);
+      fs.writeFile("./examples/logo.svg", square.makeSquare(), (err) => {
+        if (err) throw err;
+        console.log("Your logo has been generated!");
+      });
+    } else if (data.shape === "Triangle") {
+      console.log("Generating your logo now!");
+      const triangle = new Triangle(data.shapeColor, data.textColor, data.title);
+      fs.writeFile("./examples/logo.svg", triangle.makeTriangle(), (err) => {
+        if (err) throw err;
+        console.log("Your logo has been generated!");
+      });
+    } else if (data.shape === "Circle") {
+      console.log("Generating your logo now!");
+      const circle = new Circle(data.shapeColor, data.textColor, data.title);
+      fs.writeFile("./examples/logo.svg", circle.makeCircle(), (err) => {
+        if (err) throw err;
+        console.log("Your logo has been generated!");
+      });
+    } else {
+      console.log("An error has occurred");
+    }
+  });
+
+  
